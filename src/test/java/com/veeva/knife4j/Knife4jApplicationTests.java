267e28c3006ab9bac4a4b4b2d800e73630563718
Shuo Sun <shuo.sun@veeva.com>
2021-06-01 19:29:59 +0800

    Init project

---
+++ src/test/java/com/veeva/knife4j/Knife4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veeva.knife4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Knife4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
