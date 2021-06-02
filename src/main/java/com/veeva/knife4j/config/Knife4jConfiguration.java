17a09e1e1d5bf6e14ce97d580b58c11bbc346eb2
Shuo Sun <shuo.sun@veeva.com>
2021-06-02 12:31:55 +0800

    增强模式配置

--- src/main/java/com/veeva/knife4j/config/Knife4jConfiguration.java
+++ src/main/java/com/veeva/knife4j/config/Knife4jConfiguration.java
@@ -1,34 +1,50 @@
 package com.veeva.knife4j.config;
 
+import com.github.xiaoymin.knife4j.spring.extension.OpenApiExtensionResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
+import javax.annotation.Resource;
+
 @Configuration
 @EnableSwagger2WebMvc
 public class Knife4jConfiguration {
+    /**
+     * 引入Knife4j提供的扩展类
+     */
+    @Resource
+    private OpenApiExtensionResolver openApiExtensionResolver;
 
     @Bean(value = "veeva-v1")
     public Docket defaultApi2() {
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(
-                new ApiInfoBuilder().title("swagger-bootstrap-ui-demo RESTful APIs")
-                                    .description("# swagger-bootstrap-ui-demo RESTful APIs")
-                                    .termsOfServiceUrl("https://doc.xiaominfo.com/knife4j/documentation/get_start.html")
-                                    .contact("sunshuo@veeva.com")
-                                    .version("1.0")
-                                    .build())
+        String groupName = "211.3.100";
+        Contact contact = new Contact("sunshuo@veeva.com", "", "");
+        ApiInfo apiInfo = new ApiInfoBuilder().title("swagger-bootstrap-ui-demo RESTful APIs")
+                                              .description("# swagger-bootstrap-ui-demo RESTful APIs")
+                                              .termsOfServiceUrl(
+                                                      "https://doc.xiaominfo.com/knife4j/documentation/get_start.html")
+                                              .contact(contact)
+                                              .version("1.0")
+                                              .build();
+
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo)
                                                       //分组名称
-                                                      .groupName("211.3.100")
+                                                      .groupName(groupName)
                                                       .select()
                                                       //这里指定Controller扫描包路径
                                                       .apis(RequestHandlerSelectors.basePackage(
                                                               "com.veeva.knife4j.controller"))
                                                       .paths(PathSelectors.any())
-                                                      .build();
+                                                      .build()
+                                                      //赋予插件体系
+                                                      .extensions(openApiExtensionResolver.buildExtensions(groupName));
     }
 }
